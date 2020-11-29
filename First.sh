86d1f2a2ae5bff3e068bd4aee1884f36101f7929
PeterGora <68431302+PeterGora@users.noreply.github.com>
2020-11-29 22:03:56 +0100

    Update First.sh

--- First.sh
+++ First.sh
@@ -7,4 +7,6 @@ echo $MY_MESSAGE
 echo "What is your name?"
 read MY_NAME
 echo "Welcome $MY_NAME - I hope all is well, and you are ready to do this! "
+echo "I just created your own file called  ${MY_NAME}_plik  enjoy! "
+touch "${MY_NAME}_plik"
 
