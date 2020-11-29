3975cc98ab028fb2e62d0b69a0a7c457124e736d
PeterGora <68431302+PeterGora@users.noreply.github.com>
2020-11-29 21:26:23 +0100

    Update First.sh

--- First.sh
+++ First.sh
@@ -3,6 +3,6 @@
 echo Hello World	# To też jest komentarz!
 echo "Hello World"
 echo "Hello * World"
-MY_MESSAGE="Hello World"
+MY_MESSAGE="Why the World? Why not the universe? Heloo Universe!!!!"
 echo $MY_MESSAGE
 
